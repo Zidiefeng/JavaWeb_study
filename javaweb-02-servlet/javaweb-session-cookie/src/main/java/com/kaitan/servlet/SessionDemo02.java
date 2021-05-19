881936c5d1f44c856fe85547c33c61279e36acf0
Zidiefeng <35935604+Zidiefeng@users.noreply.github.com>
2021-05-18 23:56:44 -0400

    Add files via upload

--- javaweb-02-servlet/javaweb-session-cookie/src/main/java/com/kaitan/servlet/SessionDemo02.java
+++ javaweb-02-servlet/javaweb-session-cookie/src/main/java/com/kaitan/servlet/SessionDemo02.java
@@ -6,7 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-public class SessionDemo01 extends HttpServlet {
+public class SessionDemo02 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //解决乱码问题
@@ -18,21 +18,12 @@ public class SessionDemo01 extends HttpServlet {
         HttpSession session = req.getSession();
 
         //给Session中存东西
-        session.setAttribute("name",new Person("恺恺",11));
+        Person person = (Person)session.getAttribute("name");
 
-        //获取session的id
-        String sessionId = session.getId();
+        System.out.println(person.toString());
 
-        //判断是不是新的session(session是否新创建)
-        if (session.isNew()){
-            resp.getWriter().write("session创建成功，id: "+sessionId);
-        }else{
-            resp.getWriter().write("session已经存在，id: "+sessionId);
-        }
 
-        //session创建的时候做了什么
-//        Cookie cookie = new Cookie("JSESSIONID", sessionId);
-//        resp.addCookie(cookie);
+        HttpJspBase
     }
 
     @Override
